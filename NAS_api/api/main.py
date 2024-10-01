de4803cce2932c46b7cf34cf79d944a043a85a8d
Vachhanijanak <Vachhanijanak@outlook.com>
2024-10-01 14:20:05 +0530

    update

--- NAS_api/api/main.py
+++ NAS_api/api/main.py
@@ -1,7 +1,8 @@
 import os
 import sys
 
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Request, Response, Cookie
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from uvicorn import run
@@ -12,21 +13,45 @@ from database import curd
 app = FastAPI()
 curd_obj = curd()
 
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=["*"],
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
 
-class Data(BaseModel):
-	token: str
-	artical_text: str
+
+class LoginData(BaseModel):
+	email: str
+	password: str
+
+
+class RegData(BaseModel):
+	username: str
+	email: str
+	password: str
+
+
+@app.post('/create')
+async def create(data: RegData = Body()):
+	print(data.username)
+	print(data.email)
+	print(data.password)
+	return {'message': "done"}
 
 
 @app.post('/')
-async def root(query: Data = Body(...)):
-	user_obj = curd_obj.get_user(user_token=query.token)
-	if user_obj:
-		print(user_obj.email)
-		print(user_obj.password)
-		pred = 'sdjskdjskjdksjdksjd'
-		model_response = {'artical_summary': pred}
-		return JSONResponse(model_response)
+async def login(data: LoginData = Body(), ):
+	print(data.email)
+	print(data.password)
+
+	return JSONResponse({'session_id': 'wejwjkej-skdjksdjdskdks'})
+
+
+@app.post('/forgot-password')
+async def reset():
+	pass
 
 
 if '__main__' == __name__:
