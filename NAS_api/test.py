3af10c8ef082975fa56c26b9c967722bd06a0610
Vachhanijanak <Vachhanijanak@outlook.com>
2024-10-05 01:19:52 +0530

    update

--- NAS_api/test.py
+++ NAS_api/test.py
@@ -1,42 +1,102 @@
-import sys
-import os
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from database import curd, UserSession
-
-obj = curd()
-
-# from requests import post
-
-# print(post('http://127.0.0.1:8000', data={'token':'eweiweiuwe', 'artical_text':'dksjjdksjdksd'}).json())
-
-
-# temp = User()
-# temp.session = [
-# 	Session(), Session()
-# ]
-
-# obj.create_user(username='teack', email="wwwteackcom@gmail.com", password='123456789')
-
-
-# print(obj.create_session(user=obj.get_userbyid(user_id=1)))
-
-# print(obj.delete_session(token_id='etYj7AfgxFcQWJ5651haKOyCh5A6FpL4u666yn6nLh4'))
-
-
-# for _ in range(10):
-# 	obj.add_article(
-# 		'Delhi doctor shot dead by 2 boys who came for treatment at hospital',
-# 		'https://images.indianexpress.com/2024/10/nima-hospital-shooting.jpg?w=640',
-# 		'The Indian Express',
-# 		'''A doctor was shot dead in what appears to be a targeted attack at a small nursing home located in Kalindi Kunj in South East Delhi in the early hours of Thursday. According to the Delhi Police, they were alerted about the crime at around 1.45 am.
+# from database import curd
 #
-# 		When the police team arrived at the scene, they found Dr Javed Akhtar, a Unani practitioner, slumped over his desk with a gunshot wound to the left side of his head. Forensic teams from the District Crime Unit and Forensic Science Laboratory (FSL), Rohini, were called to the scene, and collected evidence for analysis, said the police.
+# obj = curd()
+
+
+from requests import get
+from bs4 import BeautifulSoup
+
+headers = {
+	'User-Agent': "Mozilla/6.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
+
+# respone = get('https://indianexpress.com', headers=headers)
 #
-# 		A police officer said their preliminary investigations revealed that two boys, approximately 16-17 years old, came to the hospital at around 1 am. One of the boys had an injured toe, which had been dressed the previous night at the same hospital by staff member Mohammad Kamil, the officer added.
+# soup = BeautifulSoup(respone.content, 'html.parser')
+# # print(soup.prettify())
 #
-# 		After the dressing, the two went to Dr Akhtar’s cabin for a prescription, and moments later, nursing staff Gajala Parveen and Kamil heard a gunshot. When Parveen went to the doctor’s cabin, she found Dr Akhtar in a pool of blood.
+# for i in soup.find_all(class_='other-article'):
+# 	link = i.find('a')['href']
+# 	break
+
+link = 'https://indianexpress.com/article/entertainment/bollywood/govinda-breaks-silence-on-shooting-himself-accidentally-answers-why-he-has-a-loaded-revolver-fame-is-a-flame-and-you-have-to-be-9603604/'
+respone = get(link, headers=headers)
+
+soup = BeautifulSoup(respone.content, 'html.parser')
+print(soup.find('h1', id='main-heading-article').text)
+print(soup.find('h2', class_='synopsis').text)
+print(soup.find('span', class_='custom-caption').img['src'])
+written_by = soup.find('div', class_="editor-date-logo")
+print(written_by.find('a').text)
+print(written_by.find('span')['content'])
+
+
+# temp = soup.find('div', class_='story_details')
 #
-# 		“The two boys also came to the hospital the previous night, got the dressing done, and left. Prima facie it is a case of targeted killing as it is unprovoked and the assailants had done recce the previous night,” said Rajesh Deo, Deputy Commissioner of Police (South East).'''
-# 	)
+# for i in temp:
+#
+# 	if i.name == 'p' and 'read more' not in i.text[:10].lower() and 'also read' not in i.text[:10].lower():
+# 		print(i.text)
+#
+# 	if i.name == 'div':
+# 		if ' '.join(i.get('class')) == 'ev-meter-content ie-premium-content-block':
+# 			break
+#
+# for i in temp.find('div', class_='ev-meter-content ie-premium-content-block'):
+#
+# 	if i.name == 'p' and 'read more' not in i.text[:10].lower() and 'also read' not in i.text[:10].lower():
+# 		print(i.text)
+#
+# 	if i.name == 'div':
+# 		if i.get('id') == 'id_newsletter_subscription':
+# 			break
+#
+#
+
+
+class IndianExpress:
+	def __init__(self):
+		pass
+
+	def entertainment(self):
+		pass
+
+	def sport(self):
+		pass
+
+	def politics(self):
+		pass
+
+	def business(self):
+		pass
+
+	def tech(self):
+		pass
+
+	def education(self):
+		pass
+
+	def research(self):
+		pass
+
+
+class Entertainment:
+	def __init__(self):
+		pass
+
+	def scrape_headline(self):
+		pass
+
+	def scrape_description(self):
+		pass
+
+	def scrape_writer(self):
+		pass
+
+	def scrape_datetime(self):
+		pass
+
+	def scrape_img(self):
+		pass
+
+	def scrape_context(self):
+		pass
